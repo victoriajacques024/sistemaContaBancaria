f04670b2f9531b5ee750367dde56178aec2b4217
Seu Nome <seuemail@exemplo.com>
2025-11-18 22:57:41 -0300

    Atualização código

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String nome = "Victoria Jacques";
+        String tipoConta = "Corrente";
+        double saldo = 1776.98;
+        int opcao = 0;
+
+
+        System.out.println("****************");
+        System.out.println("\nNome do Cliente: " + nome);
+        System.out.println("Tipo da conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("****************");
+
+        String Menu = """
+                ** Digite a opção desejada **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor
+                4 - Sair
+                
+                """;
+
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4){
+            System.out.println(Menu);
+            opcao = leitura.nextInt();
+
+            if (opcao == 1){
+                System.out.println("O saldo atualizado é: " + saldo);
+            } else if (opcao == 2 ){
+                System.out.println("Insira o valor da transferência");
+                double valor = leitura.nextDouble();
+                if (valor > saldo){
+                    System.out.println("Não há saldo suficiente.");
+                } else {
+                    saldo -= valor;
+                    System.out.println("Novo saldo: " + saldo);
+                }
+            } else if (opcao == 3 ){
+                System.out.println("Valor recebido: ");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("Novo saldo: " + saldo);
+            } else if (opcao == 4){
+                System.out.println("Sessão encerrada!");
+            } else {
+                System.out.println("Opção inválida");
+            }
+
+
+
+        }
+
+
+
     }
+
+
 }
